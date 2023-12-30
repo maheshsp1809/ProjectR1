9897e09686dfa048b053654036dd3045abca1302
maheshsp <maheshsp1809@gmail.com>
2023-12-30 21:44:14 +0530

    backend added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  useEffect(() => {
+    // Fetch tasks from the server when the component mounts
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/tasks");
+      const data = await response.json();
+      setTasks(data);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  };
+
+  const addTask = async () => {
+    if (newTask.trim() !== "") {
+      try {
+        const response = await fetch("http://localhost:5000/tasks", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ task: newTask }),
+        });
+
+        const data = await response.json();
+        setTasks(data.tasks);
+        setNewTask("");
+      } catch (error) {
+        console.error("Error adding task:", error);
+      }
+    }
+  };
+
+  const removeTask = async (index) => {
+    try {
+      const response = await fetch(`http://localhost:5000/tasks/${index}`, {
+        method: "DELETE",
+      });
+
+      const data = await response.json();
+      setTasks(data.tasks);
+    } catch (error) {
+      console.error("Error removing task:", error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do App</h1>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task} <button onClick={() => removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
