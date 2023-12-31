27ea437f236048ab0a2804ef67588389508d10d4
maheshsp <maheshsp1809@gmail.com>
2023-12-31 15:22:12 +0530

    created separate folders for Frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,20 +1,36 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:5000");
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
   useEffect(() => {
-    // Fetch tasks from the server when the component mounts
     fetchTasks();
+
+    // Listen for new task events
+    socket.on("newTask", (data) => {
+      setTasks((prevTasks) => [...prevTasks, { id: data.id, task: data.task }]);
+    });
+
+    // Listen for remove task events
+    socket.on("removeTask", (data) => {
+      setTasks((prevTasks) => prevTasks.filter((task) => task.id !== data.id));
+    });
+
+    return () => {
+      socket.disconnect();
+    };
   }, []);
 
   const fetchTasks = async () => {
     try {
       const response = await fetch("http://localhost:5000/tasks");
       const data = await response.json();
-      setTasks(data);
+      setTasks(data.tasks);
     } catch (error) {
       console.error("Error fetching tasks:", error);
     }
@@ -32,22 +48,29 @@ function App() {
         });
 
         const data = await response.json();
-        setTasks(data.tasks);
-        setNewTask("");
+
+        if (data.success) {
+          setNewTask("");
+        } else {
+          console.error("Error adding task:", data.message);
+        }
       } catch (error) {
         console.error("Error adding task:", error);
       }
     }
   };
 
-  const removeTask = async (index) => {
+  const removeTask = async (id) => {
     try {
-      const response = await fetch(`http://localhost:5000/tasks/${index}`, {
+      const response = await fetch(`http://localhost:5000/tasks/${id}`, {
         method: "DELETE",
       });
 
       const data = await response.json();
-      setTasks(data.tasks);
+
+      if (!data.success) {
+        console.error("Error removing task:", data.message);
+      }
     } catch (error) {
       console.error("Error removing task:", error);
     }
@@ -65,9 +88,10 @@ function App() {
         <button onClick={addTask}>Add Task</button>
       </div>
       <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            {task} <button onClick={() => removeTask(index)}>Remove</button>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.task}{" "}
+            <button onClick={() => removeTask(task.id)}>Remove</button>
           </li>
         ))}
       </ul>
