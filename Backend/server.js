c06371dc1c65096f60af2001a76c8a91f540fe83
maheshsp <maheshsp1809@gmail.com>
2023-12-31 14:58:01 +0530

    added websocket connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,46 +1,92 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql2");
+const http = require("http");
+const socketIO = require("socket.io");
+require("dotenv").config();
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 const PORT = process.env.PORT || 5000;
 
-// Enable CORS
 app.use(cors());
-// Enable JSON parsing
 app.use(express.json());
 
-// Todo tasks array
-let tasks = [];
-
-// Get all tasks
-app.get("/tasks", (req, res) => {
-  res.json(tasks);
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Mysql4571@",
+  database: "todo_app",
 });
 
-// Add a new task
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+  } else {
+    console.log("Connected to MySQL database");
+  }
+});
+
+// Get all tasks from the database
+app.get("/tasks", (req, res) => {
+  db.query("SELECT * FROM todos", (err, results) => {
+    if (err) {
+      console.error("Error fetching tasks from the database:", err);
+      res
+        .status(500)
+        .json({ success: false, message: "Internal Server Error" });
+    } else {
+      const tasks = results.map((row) => ({ id: row.id, task: row.task }));
+      res.json({ tasks });
+    }
+  });
+});
+
+// Add a new task to the database
 app.post("/tasks", (req, res) => {
   const { task } = req.body;
-  console.log("Received task:", task);
+
   if (task.trim() !== "") {
-    tasks.push(task);
-    res.json({ success: true, tasks });
+    db.query("INSERT INTO todos (task) VALUES (?)", [task], (err, result) => {
+      if (err) {
+        console.error("Error adding task to the database:", err);
+        res
+          .status(500)
+          .json({ success: false, message: "Internal Server Error" });
+      } else {
+        const newTaskId = result.insertId;
+        io.emit("newTask", { id: newTaskId, task }); // Notify clients about the new task
+        res.json({ success: true, message: "Task added successfully" });
+      }
+    });
   } else {
     res.status(400).json({ success: false, message: "Task cannot be empty." });
   }
 });
 
-// Remove a task
-app.delete("/tasks/:index", (req, res) => {
-  const { index } = req.params;
-  if (index >= 0 && index < tasks.length) {
-    tasks.splice(index, 1);
-    res.json({ success: true, tasks });
-  } else {
-    res.status(404).json({ success: false, message: "Task not found." });
-  }
+// Remove a task from the database
+app.delete("/tasks/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.query("DELETE FROM todos WHERE id = ?", [id], (err) => {
+    if (err) {
+      console.error("Error removing task from the database:", err);
+      res
+        .status(500)
+        .json({ success: false, message: "Internal Server Error" });
+    } else {
+      io.emit("removeTask", { id }); // Notify clients about the removed task
+      res.json({ success: true, message: "Task removed successfully" });
+    }
+  });
 });
 
-// Start the server
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log("Client connected");
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
