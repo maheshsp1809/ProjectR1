1ad0f8b128736ddc97ecae79477b83e3401b7772
maheshsp <maheshsp1809@gmail.com>
2024-01-04 23:33:14 +0530

    port

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -116,5 +116,5 @@ app.options("*", cors());
 
 // Start the server
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on  default http://localhost:${PORT}`);
 });
